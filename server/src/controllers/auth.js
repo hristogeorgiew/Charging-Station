bfc14afb64b88caea1b7036da53fb9e3f0f6c32a
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-11-16 11:51:58 +0200

    Create routes and controllers (register, login, resetPassword, forgotPassword)

---
+++ server/src/controllers/auth.js
@@ -0,0 +1,15 @@
+exports.register = (req, res, next) => {
+    res.send('hello')
+}
+
+exports.login = (req, res, next) => {
+    
+}
+
+exports.forgotPassword = (req, res, next) => {
+    
+}
+
+exports.resetPassword = (req, res, next) => {
+    
+}
